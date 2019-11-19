bc53e539c0aff928d959532584f5e27912a2ca10
Mohammad <rahmanmt@umich.edu>
2019-11-19 08:57:30 -0500

    Updates

---
+++ precache-manifest.150f982edd05a192f48b157d8c22b445.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa91de15e65dd4b0f0e9de96647c7b48",
+    "url": "/Hotel-app-GP/index.html"
+  },
+  {
+    "revision": "2db66130e80dd62bcbfa",
+    "url": "/Hotel-app-GP/static/css/main.6d2fb2cb.chunk.css"
+  },
+  {
+    "revision": "aacdb17550265a5b684a",
+    "url": "/Hotel-app-GP/static/js/2.b6aa720b.chunk.js"
+  },
+  {
+    "revision": "2db66130e80dd62bcbfa",
+    "url": "/Hotel-app-GP/static/js/main.94b111fe.chunk.js"
+  },
+  {
+    "revision": "50383b1143c7fdcdcde6",
+    "url": "/Hotel-app-GP/static/js/runtime-main.c2d6e6bd.js"
+  },
+  {
+    "revision": "0e375675ddb6a32150a12fe96ff9356a",
+    "url": "/Hotel-app-GP/static/media/bed.0e375675.jpg"
+  },
+  {
+    "revision": "6d0d4b84db2b9d42c8f576cb45ab4b61",
+    "url": "/Hotel-app-GP/static/media/beds.6d0d4b84.jpg"
+  },
+  {
+    "revision": "09f54f6b82654d59c759f5a5f470ba43",
+    "url": "/Hotel-app-GP/static/media/dish.09f54f6b.png"
+  },
+  {
+    "revision": "66793b7bed87cd237e1b3fdf221df9da",
+    "url": "/Hotel-app-GP/static/media/hair-dryer.66793b7b.png"
+  },
+  {
+    "revision": "a89ec1f89d1609e1a559731b91a84ce1",
+    "url": "/Hotel-app-GP/static/media/homePic.a89ec1f8.jpg"
+  },
+  {
+    "revision": "f3ed208f73b3dbc38866179f012f8076",
+    "url": "/Hotel-app-GP/static/media/introPic.f3ed208f.jpg"
+  },
+  {
+    "revision": "791be8a9b0103e665c8733a2604e3be9",
+    "url": "/Hotel-app-GP/static/media/logo.791be8a9.jpg"
+  },
+  {
+    "revision": "7a82830ae575e80fdf40482cdc0c8b5f",
+    "url": "/Hotel-app-GP/static/media/phone.7a82830a.png"
+  },
+  {
+    "revision": "77ce820365559dbd04ce81495e5dde33",
+    "url": "/Hotel-app-GP/static/media/pool.77ce8203.png"
+  },
+  {
+    "revision": "c0987635fb0e9c0ce752f16f63af82f4",
+    "url": "/Hotel-app-GP/static/media/pool.c0987635.jpg"
+  },
+  {
+    "revision": "b08fae2c5b5dfe765d29837dfbb0db5d",
+    "url": "/Hotel-app-GP/static/media/room2.b08fae2c.jpg"
+  },
+  {
+    "revision": "c685ccc6140c5054f563fd945b0813f8",
+    "url": "/Hotel-app-GP/static/media/roomImageThree.c685ccc6.jpg"
+  },
+  {
+    "revision": "c2441f45fb1c0c338b3eb91672e27bb5",
+    "url": "/Hotel-app-GP/static/media/roomImgOne.c2441f45.jpg"
+  },
+  {
+    "revision": "59b2efc65bd5a5c61695c7ee21994e9e",
+    "url": "/Hotel-app-GP/static/media/roomImgTwo.59b2efc6.jpg"
+  },
+  {
+    "revision": "afaf92850f1de51d2571c1a8cd2569ac",
+    "url": "/Hotel-app-GP/static/media/safebox.afaf9285.png"
+  },
+  {
+    "revision": "1529bc71ec4c497b80941165fa680823",
+    "url": "/Hotel-app-GP/static/media/spa.1529bc71.jpg"
+  },
+  {
+    "revision": "247948948daef1c23926fc50117a9627",
+    "url": "/Hotel-app-GP/static/media/testImage.24794894.jpg"
+  },
+  {
+    "revision": "7dca2effca61a09ecfb13dcbbe214b74",
+    "url": "/Hotel-app-GP/static/media/wifi.7dca2eff.png"
+  }
+]);
